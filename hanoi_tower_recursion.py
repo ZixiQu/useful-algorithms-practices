e56ccb1f13dee75c03d703017d4e11f30dfe6049
Zixi Qu <k4nyequ@gmail.com>
2020-10-23 16:46:13 -0400

    nothing fancy, same as last commit

--- hanoi_tower_recursion.py
+++ hanoi_tower_recursion.py
@@ -10,6 +10,26 @@ def hanoi(n: int, a, b, c):
 		hanoi(n-1, b, a, c)
 
 
+def move_tuple(a, b):
+	return (a, "To" ,b)
+
+def honoi_list(n, a, b, c) -> list:
+	if n == 0:
+		return []
+	else:
+		lst = []
+		lst.extend(honoi_list(n-1, a, c, b))
+		lst.append(move_tuple(a, c))
+		lst.extend(honoi_list(n-1, b, a ,c))
+		return lst
+
+
 
 if __name__ == "__main__":
-	hanoi(5, "A", "B", "C")
+	# print(move_tuple("A", "B"), type(move_tuple("A", "B")))
+	result = honoi_list(6, "A", "B", "C") 
+	step = 1
+	while result:
+		thing = result.pop(0)
+		print(thing, step)
+		step += 1
