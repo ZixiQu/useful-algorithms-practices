e9a1c6df1c8590d8d80cb43f29ff0e40bce2691f
Zixi Qu <k4nyequ@gmail.com>
2020-10-28 16:29:15 -0400

    added gcd.py

---
+++ gcd.py
@@ -0,0 +1,16 @@
+def gcd(a, b):
+    """ Return the greatest common devider(gcd) between a and b using
+    Euclidean Algorithm with recursion
+
+    pre: a and b are integeres 1 <= b <= a
+    post: returns the greatest common divisor of a and b
+    """
+    if a == 1 and b == 1:
+        return 1
+    elif a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+if __name__ == "__main__":
+    print(gcd(100, 24))
