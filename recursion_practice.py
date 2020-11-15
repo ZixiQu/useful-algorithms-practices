02de87453d433e823f9d6962e569f0c5ca304eed
Zixi Qu <k4nyequ@gmail.com>
2020-11-15 18:07:22 -0500

    add something not important

--- recursion_practice.py
+++ recursion_practice.py
@@ -2,7 +2,7 @@
 # [(W), (D), (L)]
 
 # input: [(1,2), (3,4)]
-# [(W, W), (D, W), (L, W), (W, D), (D, D), (L, D), (W, L), (D, L ), (L, L)]
+# [(W, W), (D, W), (L, W), (W, D), (D, D), (L, D), (W, L), (D, L), (L, L)]
 
 from typing import List
 
@@ -11,23 +11,25 @@ def solve(input):
         return list of tuples contains possiblity of possiblity of games
     """
     if len(input) == 1:
-        return [["W"], ["D"] ,["L"]]
+        take = input.pop()
+        return [[take, "W"], [take, "D"] ,[take, "L"]]
     else:
         current = input.pop()
         next_level = solve(input)
         result = []
         for each in next_level:
-            a = each + ["W"]
+            a = each + [current, "W"]
             result.append(a)
 
         for each in next_level:
-            a = each + ["D"]
+            a = each + [current, "D"]
             result.append(a)
 
         for each in next_level:
-            a = each + ["L"]
+            a = each + [current, "L"]
             result.append(a)
     return result
 
+
 if __name__ == "__main__":
-    print(solve([[1,2], [3,4]]))
+    print(solve([(1,2), (3,4), (5, 6)]))
