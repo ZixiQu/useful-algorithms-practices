e0e92fb86a526fb60649e4e58b5c05fb0430a005
Zixi Qu <k4nyequ@gmail.com>
2020-12-18 17:31:22 -0500

    add some random things I created from last commit

---
+++ str.split().py
@@ -0,0 +1,14 @@
+def split(s: str, sep=" \n\t"):
+    """ simimulating python str.split()"""
+    print(sep)
+    result = [""]
+    for each in s:
+        if not each in sep:
+            result[-1] += each
+        elif each in sep and result[-1] != "":
+            result.append("")
+    return result
+
+if __name__ == "__main__":
+    print(split("something, hello, world", sep=","))
+
