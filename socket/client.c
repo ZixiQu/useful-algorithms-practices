898972643755d5891413a787c068b9981232a1b0
unknown <k4nyequ@gmail.com>
2021-04-01 00:05:12 -0400

    implemented server-client framework

--- socket/client.c
+++ socket/client.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 
 #define PORT 54321
+#define MAXSIZE 100
 
 int main(){
     int client_soc = socket(AF_INET, SOCK_STREAM, 0);
@@ -19,7 +20,7 @@ int main(){
     server.sin_port = htons(54321);
 
     struct addrinfo *result;
-    getaddrinfo("142.1.46.85", NULL, NULL, &result);
+    getaddrinfo("192.168.0.14", NULL, NULL, &result);
     server.sin_addr = ((struct sockaddr_in *) result->ai_addr)->sin_addr;
 
     connect(client_soc, (struct sockaddr *)&server, sizeof(struct sockaddr_in));
@@ -28,7 +29,30 @@ int main(){
 
     read(client_soc, buf, 100);
     buf[17] = '\0';
-    printf("information from server: %s\n", buf);
+    printf("something%s \n", buf);
+    
+    fd_set fdset;
+    buf[0] = '\0';
+    while (1){
+        FD_ZERO(&fdset);
+        FD_SET(client_soc, &fdset);
+        FD_SET(STDIN_FILENO, &fdset);
 
-    // write();
+        int ready = select(client_soc + 1, &fdset, NULL, NULL, NULL);
+        if (ready == -1){
+            perror("select");\
+            exit(1);
+        }
+        if (FD_ISSET(STDIN_FILENO, &fdset)){
+            int num_byte = read(STDIN_FILENO, buf, MAXSIZE);
+            write(client_soc, buf, num_byte);
+        }
+        if (FD_ISSET(client_soc, &fdset)){
+            int num_byte = read(client_soc, buf, MAXSIZE);
+            write(STDIN_FILENO, buf, num_byte);   
+        }
+        buf[0] = '\0';
+    }
+
+    return 0;
 }
--- socket/server.c
+++ socket/server.c
@@ -9,6 +9,7 @@
 
 
 #define PORT 54321
+#define MAXSIZE 100
 
 int main(){
     int server_soc = socket(AF_INET, SOCK_STREAM, 0);
@@ -37,8 +38,31 @@ int main(){
         close(client_fd);
         exit(1);
     }
-
+    printf("connection succeed");
     write(client_fd, "hi from client \r\n", 17);
+
+    fd_set fdset;
+    char buf[MAXSIZE];
+    while(1){
+        FD_ZERO(&fdset);
+        FD_SET(STDIN_FILENO, &fdset);
+        FD_SET(client_fd, &fdset);
+
+        int ready = select(client_fd + 1, &fdset, NULL, NULL, NULL);
+        if (ready == -1){
+            perror("select");
+            exit(1);
+        }
+        if (FD_ISSET(STDIN_FILENO, &fdset)){
+            int num_bytes = read(STDIN_FILENO, buf, MAXSIZE);
+            write(client_fd, buf, num_bytes);
+        }
+        if (FD_ISSET(client_fd, &fdset)){
+            int num_bytes = read(client_fd, buf, MAXSIZE);
+            write(STDIN_FILENO, buf, num_bytes);
+        }
+        buf[0] = '\0';
+    }
 
     return 0;
 
