1808f969956a12ed62bc6a6b23b693e8f84811ee
somehacker <k4nyequ@gmail.com>
2022-11-29 01:51:43 -0500

    clean debug message, make output message more readable

--- folder_size.py
+++ folder_size.py
@@ -1,6 +1,8 @@
 import os
 import pprint
 
+DEBUG = 0
+
 def _total_size(source):
         total_size = os.path.getsize(source)
         for item in os.listdir(source):
@@ -11,29 +13,36 @@ def _total_size(source):
                 try:
                     total_size += _total_size(itempath)
                 except:
-                    print(f"{itempath} permission denied")
+                    if DEBUG: print(f"{itempath} permission denied")
         return total_size
 
 def main():
-    basepath = 'C:/'
+    basepath = 'F:/'
     information = {}
+    folder_total_size = 0
+    file_total_size = 0
     for each in os.listdir(basepath):
-        print(each)
+        # print(each)
         each = f"{basepath}/{each}" 
         # print(each, os.path.isdir(each))
         if os.path.isdir(each):
             try:
-                information[each] = _total_size(each) / 1024**3
+                folder_size = _total_size(each) / 1024**3
+                information[each] = folder_size
+                folder_total_size += folder_size
                 # print(f"{each}: {_total_size(each) / 1024**3} GB")
             except:
-                print(f"{each} permission denied 2")
+                if DEBUG: print(f"{each} permission denied 2")
         else:
-            pass
-            # print(f"{each} is a file")
+            file_total_size += os.path.getsize(each)
     print(f"current folder: {basepath}")
+    print("="*30)
     for path, size in sorted(information.items(), key=lambda x:x[1], reverse=True):
         print(f"{path.split('/')[-1]}: {size} GB")
+    print("==============SUMMARY=============")
+    print(f"folder_total_size: {folder_total_size} GB")
+    print(f"file_total_size: {file_total_size / 1024**3} GB")
 
 if __name__ == "__main__":
-    # main()
-    print(os.getcwd())
+    main()
+    # print(os.getcwd())
